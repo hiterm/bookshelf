183fc9f3b4d7d1463d605cce3c1edb94c52bdfc0
htlsne <htlsne23@gmail.com>
2020-06-15 21:04:33 +0900

    Render book list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+type Book = {
+  title: string;
+};
+
+const BookList = (props: { list: Book[] }) => (
+  <ul>
+    {props.list.map((book) => (
+      <li>{book.title}</li>
+    ))}
+  </ul>
+);
+
+const App: React.FC<{}> = () => {
+  const books = [
+    {
+      title: 'Book1',
+    },
+    {
+      title: 'Book2',
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookList list={books} />
     </div>
   );
-}
+};
 
 export default App;
