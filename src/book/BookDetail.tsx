00731f371004af84fbb8ca7a44d14ad8e4fcc4fd
htlsne <htlsne23@gmail.com>
2020-07-09 23:21:25 +0900

    Make detail form work

--- src/book/BookDetail.tsx
+++ src/book/BookDetail.tsx
@@ -4,6 +4,7 @@ import { Formik, Field, Form } from 'formik';
 import Button from '@material-ui/core/Button';
 import { db } from '../Firebase';
 import { Book, firebaseDocToBook } from './schema';
+import { TextField } from 'formik-material-ui';
 
 const BookDetail: React.FC<{}> = () => {
   const { id } = useParams();
@@ -22,22 +23,32 @@ const BookDetail: React.FC<{}> = () => {
       });
   });
 
+  if (book === null) {
+    return <div>Loading or not found.</div>;
+  }
+
   return (
     <React.Fragment>
-      <div>書名: {book?.title}</div>
-      <div>著者：{book?.authors.join(', ')}</div>
-      <div>優先度：{book?.priority}</div>
+      <div>書名: {book.title}</div>
+      <div>著者：{book.authors.join(', ')}</div>
+      <div>優先度：{book.priority}</div>
       <Formik
-        initialValues={{ priority: 50 }}
+        initialValues={{ priority: book.priority }}
+        /* validationSchema={bookSchema} */
         onSubmit={(values) => {
-          let docRef = db.collection('books').doc(book?.id);
+          let docRef = db.collection('books').doc(book.id);
           docRef.update({
             priority: values.priority,
           });
         }}
       >
         <Form>
-          <Field name="priority" type="number" />
+          <Field
+            component={TextField}
+            name="priority"
+            type="number"
+            label="優先度"
+          />
           <Button variant="contained" color="primary" type="submit">
             更新
           </Button>
