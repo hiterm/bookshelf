865b30c5bd860b2b90c2cc3123aeb074945feb58
htlsne <htlsne23@gmail.com>
2020-08-02 22:54:54 +0900

    Add profile icon

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -7,8 +7,22 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 export const AppBar: React.FC = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <MuiAppBar position="static" css={{ marginBottom: 50 }}>
       <Toolbar>
@@ -22,7 +36,28 @@ export const AppBar: React.FC = () => {
             Bookshelf
           </MuiLink>
         </Typography>
-        <Button color="inherit">Login</Button>
+        {
+          // <Button color="inherit">Login</Button>
+        }
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+          onClick={handleClick}
+        >
+          <AccountCircle />
+        </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </MuiAppBar>
   );
