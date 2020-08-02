4ed9aa9ef2cb322429778d8fdd589f0fc09bcc01
htlsne <htlsne23@gmail.com>
2020-08-02 23:03:12 +0900

    Update sign in screen

--- src/SignInScreen.tsx
+++ src/SignInScreen.tsx
@@ -12,8 +12,6 @@ const uiConfig = {
 export const SignInScreen: React.FC<{}> = () => {
   return (
     <div>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </div>
   );
