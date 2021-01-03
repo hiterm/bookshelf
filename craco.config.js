dd1365b6648eaeb401b1bc3fd7550c7a5b1aff82
htlsne <htlsne23@gmail.com>
2021-01-03 16:05:39 +0900

    Remove comments

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   babel: {
     presets: ["@emotion/babel-preset-css-prop"]
-    // presets: [
-    //   [
-    //     '@babel/preset-react',
-    //     { runtime: 'automatic', importSource: '@emotion/react' },
-    //   ],
-    // ],
-    // plugins: ['@emotion/babel-plugin'],
   },
 };
