73b450ce73361ad1be8fe3c337f0f0fd971542bf
htlsne <htlsne23@gmail.com>
2022-02-05 19:08:53 +0900

    Write an example hooks test

--- src/features/books/BookForm.test.tsx
+++ src/features/books/BookForm.test.tsx
@@ -1,5 +1,8 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { useBookForm } from './BookForm';
+import { renderHook } from '@testing-library/react-hooks';
+import { BookBaseType } from './schema';
 
 const App: React.VFC = () => {
   return <h1>Hello, World!</h1>;
@@ -8,4 +11,25 @@ const App: React.VFC = () => {
 test('renders a message', () => {
   const { container, getByText } = render(<App />);
   expect(getByText('Hello, World!')).toBeInTheDocument();
+});
+
+describe('useBookForm', () => {
+  test('works', () => {
+    const emptyBook: BookBaseType = {
+      title: '',
+      authors: [''],
+      read: false,
+      owned: false,
+      priority: 50,
+    };
+
+    const { result } = renderHook(() =>
+      useBookForm({
+        onSubmit: () => {},
+        initialValues: emptyBook,
+      })
+    );
+    const { container, getByText, getAllByText } = render(result.current.renderForm());
+    expect( getAllByText('書名')[0]).toBeInTheDocument();
+  });
 });
