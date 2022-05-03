233a823a145d9ab766c995ceb5acfa43454702e0
hiterm <hiterm23@gmail.com>
2022-05-03 15:08:08 +0900

    Rename file

--- src/pages/MainRoutes.tsx
+++ src/pages/MainRoutes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { SignInScreen } from '../SignInScreen';
 import { BookRouter } from './books/BookRoutes';
-import { AuthorIndexPage } from './authors/Author';
+import { AuthorIndexPage } from './authors/AuthorIndexPage';
 
 export const MainRoutes: React.FC = () => (
   <Switch>
