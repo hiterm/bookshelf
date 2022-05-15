c8ca5e1c2da0989482a3edd03bc48d83ef2c6467
hiterm <hiterm23@gmail.com>
2022-05-15 15:24:15 +0900

    Show books

--- src/pages/books/BookIndexPage.tsx
+++ src/pages/books/BookIndexPage.tsx
@@ -1,50 +1,33 @@
 import React, { useMemo } from 'react';
 import { BookAddButton } from '../../features/books/BookAddButton';
 import { BookList } from '../../features/books/BookList';
-import { Book } from '../../features/books/schema';
+import { GraphQLBook } from '../../features/books/schema';
 import { useBooksQuery } from '../../generated/graphql';
 
-const BookIndexPage: React.FC = (props) => {
+const BookIndexPage: React.FC = () => {
   const context = useMemo(() => ({ additionalTypenames: ['Book'] }), []);
   const [result, _reexecuteQuery] = useBooksQuery({ context });
   const { data, fetching, error } = result;
+
+  if (error != null) {
+    return <>{JSON.stringify(error)}</>;
+  }
 
   if (fetching || data == null) {
     return <>loading</>;
   }
 
+  const books: GraphQLBook[] = data.books.map((book) => ({
+    ...book,
+    createdAt: new Date(1000 * book.createdAt),
+    updatedAt: new Date(1000 * book.updatedAt),
+  }));
+
   return (
     <React.Fragment>
       <h2>一覧</h2>
       <BookAddButton />
-      {data.books.map((book) => (
-        <>
-          <div>{book.id}</div>
-          <div>{book.title}</div>
-          <div>{JSON.stringify(book.authors)}</div>
-        </>
-      ))}
-      {/* <BookList list={data.books} /> */}
-      <BookList
-        list={[
-          {
-            id: 'a',
-            title: 'title',
-            authors: [
-              { id: 'id1', name: 'author1' },
-              { id: 'id2', name: 'author2' },
-            ],
-            isbn: 'aa',
-            read: false,
-            owned: false,
-            priority: 50,
-            format: 'E_BOOK',
-            store: 'KINDLE',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-        ]}
-      />
+      <BookList list={books} />
     </React.Fragment>
   );
 };
