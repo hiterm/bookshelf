2432bb0874f8cb1278d32ec4f3d5fc008496e7b2
hiterm <hiterm23@gmail.com>
2022-05-22 22:31:01 +0900

    Add side menu

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,5 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { Menu as MenuIcon } from '@mui/icons-material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material';
 import MuiAppBar from '@mui/material/AppBar';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
@@ -9,10 +17,43 @@ import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
+
+type MyDrawlerProps = { open: boolean; onClose: () => void };
+
+const MyDrawler: React.FC<MyDrawlerProps> = (props) => {
+  const history = useHistory();
+
+  const handleBookClick = () => {
+    history.push(`/books`);
+    props.onClose();
+  };
+  const handleAuthorClick = () => {
+    history.push(`/authors`);
+    props.onClose();
+  };
+
+  return (
+    <Drawer anchor="left" open={props.open} onClose={props.onClose}>
+      <List sx={{ minWidth: 200 }}>
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleBookClick}>
+            <ListItemText primary="本" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleAuthorClick}>
+            <ListItemText primary="著者" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
 
 export const AppBar: React.FC = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [isDrawerOpen, setIsDrawlerOpen] = React.useState(false);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -30,7 +71,25 @@ export const AppBar: React.FC = () => {
 
   return (
     <MuiAppBar position="static" sx={{ marginBottom: 5 }}>
+      <MyDrawler
+        open={isDrawerOpen}
+        onClose={() => {
+          setIsDrawlerOpen(false);
+        }}
+      />
       <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+          onClick={() => {
+            setIsDrawlerOpen(true);
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
         <Typography variant="h5" color="inherit" sx={{ flexGrow: 1 }}>
           <MuiLink
             component={RouterLink}
