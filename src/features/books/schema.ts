2ce180209af2a59ae68473fd9694d404f94e3769
hiterm <hiterm23@gmail.com>
2022-05-28 21:58:07 +0900

    Remove usage of yup

--- src/features/books/schema.ts
+++ src/features/books/schema.ts
@@ -1,55 +1,4 @@
-import * as yup from 'yup';
 import { BookFormat, BookStore, BooksQuery } from '../../generated/graphql';
-
-const bookBaseSchema = yup
-  .object({
-    title: yup.string().required(),
-    authors: yup.array().of(yup.string().required()).required().default([]),
-    isbn: yup.string().matches(/^(\d-?){12}\d$/, { excludeEmptyString: true }),
-    read: yup.boolean().required().default(false),
-    priority: yup.number().integer().min(0).max(100).required().default(50),
-    format: yup.string().oneOf(['eBook', 'Printed']),
-    store: yup.string().oneOf(['Kindle']),
-    owned: yup.boolean().defined().default(false),
-  })
-  .required();
-
-const bookSchema = bookBaseSchema.shape({
-  id: yup.string().required(),
-  createdAt: yup
-    .date()
-    .required()
-    .default(() => new Date()),
-  updatedAt: yup
-    .date()
-    .required()
-    .default(() => new Date()),
-});
-
-export interface OldBookBaseType {
-  title: string;
-  authors: string[];
-  isbn?: string;
-  read: boolean;
-  owned: boolean;
-  priority: number;
-  format?: 'eBook' | 'Printed';
-  store?: 'Kindle';
-}
-
-export interface OldBook {
-  title: string;
-  authors: string[];
-  isbn?: string;
-  read: boolean;
-  owned: boolean;
-  priority: number;
-  format?: 'eBook' | 'Printed';
-  store?: 'Kindle';
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
 
 export type Author = {
   id: string;
@@ -113,5 +62,3 @@ export const displayBookStore = (store: BookStore): string => {
       return 'Unknown';
   }
 };
-
-export { bookBaseSchema as bookFormSchema, bookSchema };
