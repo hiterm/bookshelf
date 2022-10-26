7be0c6c5f6300be0a25b14801c749afe51688773
hiterm <hiterm23@gmail.com>
2022-10-26 23:36:00 +0900

    Fix: Navbar link

--- src/compoments/Navbar.tsx
+++ src/compoments/Navbar.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export const Navbar: React.FC = () => {
   return (
     <MantineNavbar p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-      <NavLink label="本" component={Link} to="books" />
-      <NavLink label="著者" component={Link} to="authors" />
+      <NavLink label="本" component={Link} to="/books" />
+      <NavLink label="著者" component={Link} to="/authors" />
     </MantineNavbar>
   );
 };
