c64d324db4affd89858004a448f725060238a9a0
hiterm <hiterm23@gmail.com>
2022-10-31 18:53:45 +0900

    feat: Improve ShowBoolean

--- src/features/books/BookDetailShow.tsx
+++ src/features/books/BookDetailShow.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Center, Modal, Text, Title } from "@mantine/core";
+import { Box, Button, Center, Modal, Text, ThemeIcon, Title } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
-import { IconCheck } from "@tabler/icons";
+import { IconCheck, IconX } from "@tabler/icons";
 import dayjs from "dayjs";
 import React, { useState } from "react";
 import { Link, useHistory, useRouteMatch } from "react-router-dom";
@@ -34,7 +34,19 @@ const BookDetailShowItem: React.FC<{
   );
 };
 
-const ShowBoolean: React.FC<{ flag: boolean }> = ({ flag }) => <IconCheck color={flag ? "green" : undefined} />;
+const ShowBoolean: React.FC<{ flag: boolean }> = ({ flag }) => {
+  return flag
+    ? (
+      <ThemeIcon size="sm" color="green">
+        <IconCheck />
+      </ThemeIcon>
+    )
+    : (
+      <ThemeIcon size="sm" color="grey">
+        <IconX />
+      </ThemeIcon>
+    );
+};
 
 const DeleteButton: React.FC<{ book: Book }> = ({ book }) => {
   const [open, setOpen] = useState(false);
