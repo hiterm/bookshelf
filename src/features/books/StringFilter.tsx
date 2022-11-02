242bfd1808d0080b7ef3b2687172fb9add96ed7a
hiterm <hiterm23@gmail.com>
2022-11-02 23:18:52 +0900

    feat: Debounce StringFilter

--- src/features/books/StringFilter.tsx
+++ src/features/books/StringFilter.tsx
@@ -1,13 +1,23 @@
 import { TextInput } from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
 import { Column } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
 
 export type StringFilterProps<TData, TValue> = { column: Column<TData, TValue> };
 
 export const StringFilter = <TData, TValue>({ column }: StringFilterProps<TData, TValue>) => {
+  const [value, setValue] = useState<string>((column.getFilterValue() ?? "") as string);
+  const [debouncedValue] = useDebouncedValue(value, 1000);
+
+  useEffect(() => {
+    column.setFilterValue(debouncedValue);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue]);
+
   return (
     <TextInput
-      value={column.getFilterValue() as string ?? ""}
-      onChange={event => column.setFilterValue(event.target.value)}
+      value={value}
+      onChange={event => setValue(event.target.value)}
     />
   );
 };
