9cb85bc09cc4ae0cd6a2781e5c69bc44e84c33a9
hiterm <hiterm23@gmail.com>
2022-11-03 13:41:10 +0900

    refactor: Separate Filter components

--- src/features/books/Filter.tsx
+++ src/features/books/Filter.tsx
@@ -1,7 +1,8 @@
-import { Select } from "@mantine/core";
 import { Column, Table as ReactTable } from "@tanstack/react-table";
 import { AuthorsFilter } from "./AuthorsFilter";
-import { BOOK_FORMAT_VALUE, BOOK_STORE_VALUE, displayBookFormat, displayBookStore } from "./schema";
+import { BooleanFilter } from "./BooleanFilter";
+import { FormatFilter } from "./FormatFilter";
+import { StoreFilter } from "./StoreFilter";
 import { StringFilter } from "./StringFilter";
 
 type FilterProps<TData, TValue> = { column: Column<TData, TValue>; table: ReactTable<TData> };
@@ -11,49 +12,11 @@ export const Filter = <TData, TValue>({ column }: FilterProps<TData, TValue>): J
     case "string":
       return <StringFilter column={column} />;
     case "boolean":
-      return (
-        <Select
-          data={["-", "true", "false"]}
-          value={String(column.getFilterValue() ?? "-")}
-          onChange={value => {
-            if (value === "true") {
-              column.setFilterValue(true);
-            } else if (value === "false") {
-              column.setFilterValue(false);
-            } else if (value === "-") {
-              column.setFilterValue(undefined);
-            }
-          }}
-        />
-      );
+      return <BooleanFilter column={column} />;
     case "format":
-      return (
-        <Select
-          data={[
-            { value: "", label: "-" },
-            ...BOOK_FORMAT_VALUE.map((format) => ({
-              value: format,
-              label: displayBookFormat(format),
-            })),
-          ]}
-          value={column.getFilterValue() as string ?? ""}
-          onChange={value => column.setFilterValue(value)}
-        />
-      );
+      return <FormatFilter column={column} />;
     case "store":
-      return (
-        <Select
-          data={[
-            { value: "", label: "-" },
-            ...BOOK_STORE_VALUE.map((format) => ({
-              value: format,
-              label: displayBookStore(format),
-            })),
-          ]}
-          value={column.getFilterValue() as string ?? ""}
-          onChange={value => column.setFilterValue(value)}
-        />
-      );
+      return <StoreFilter column={column} />;
     case "authors":
       return (
         <AuthorsFilter
---
+++ src/features/books/StoreFilter.tsx
@@ -0,0 +1,21 @@
+import { Select } from "@mantine/core";
+import { Column } from "@tanstack/react-table";
+import { BOOK_STORE_VALUE, displayBookStore } from "./schema";
+
+export type StoreFilterProps<TData, TValue> = { column: Column<TData, TValue> };
+
+export const StoreFilter = <TData, TValue>({ column }: StoreFilterProps<TData, TValue>): JSX.Element => {
+  return (
+    <Select
+      data={[
+        { value: "", label: "-" },
+        ...BOOK_STORE_VALUE.map((format) => ({
+          value: format,
+          label: displayBookStore(format),
+        })),
+      ]}
+      value={column.getFilterValue() as string ?? ""}
+      onChange={value => column.setFilterValue(value)}
+    />
+  );
+};
