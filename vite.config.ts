428a497304e9e91c7c9bd3d966f6dc9ee01bcc25
hiterm <hiterm23@gmail.com>
2025-07-09 23:11:34 +0900

    Enable tsr vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
 
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vitePluginChecker from "vite-plugin-checker";
@@ -8,7 +9,15 @@ import vitePluginChecker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [react(), vitePluginChecker({ typescript: true })],
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+    vitePluginChecker({ typescript: true }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
