363bf5ddfdcb8fbd2f83113c89cfcd9c422e42da
hiterm <hiterm23@gmail.com>
2025-07-10 22:46:13 +0900

    Redirect `/` to `/books/`

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: RouteComponent,
+  beforeLoad: () => {
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw redirect({ to: "/books" });
+  },
 });
-
-function RouteComponent() {
-  return <div>Hello !</div>;
-}
