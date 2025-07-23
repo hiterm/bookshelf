2e4efecbdd5730d1f0f9f8471915cf7612a1dddb
hiterm <hiterm23@gmail.com>
2025-07-23 23:03:58 +0900

    Rewrite books/$id

--- src/routes/books/$id.tsx
+++ src/routes/books/$id.tsx
@@ -1,12 +1,43 @@
+// Book detail page with graphql-request loader (urql removed)
+
 import { Center, Loader } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
 import React from "react";
 import { BookDetailShow } from "../../features/books/BookDetailShow";
 import { graphQlBookToBook } from "../../features/books/entity/Book";
-import { useBookQuery } from "../../generated/graphql";
+import { graphql } from "../../generated/gql";
+
+export const BookQueryDocument = graphql(/* GraphQL */ `
+  query book($bookId: ID!) {
+    book(id: $bookId) {
+      id
+      title
+      authors {
+        id
+        name
+      }
+      isbn
+      read
+      owned
+      priority
+      format
+      store
+      createdAt
+      updatedAt
+    }
+  }
+`);
 
 export const Route = createFileRoute("/books/$id")({
+  loader: async ({ params, context }) => {
+    const data = await context.graphql.requestWithAuth(
+      BookQueryDocument,
+      { bookId: params.id }
+    );
+    return data.book;
+  },
   component: RouteComponent,
+  pendingComponent: Loader,
 });
 
 function RouteComponent() {
@@ -14,30 +45,10 @@ function RouteComponent() {
 }
 
 const BookDetailPage: React.FC = () => {
-  const { id } = Route.useParams();
-
-  const [result, _reexecuteQuery] = useBookQuery({ variables: { bookId: id } });
-  const { data, fetching, error } = result;
-
-  if (error != null) {
-    return <>{JSON.stringify(error)}</>;
-  }
-
-  if (fetching || data == null) {
-    return (
-      <Center>
-        <Loader />
-      </Center>
-    );
-  }
-
-  const graphqlBook = data.book;
-
-  if (graphqlBook == null) {
+  const graphqlBook = Route.useLoaderData();
+  if (!graphqlBook) {
     return <div>Not found.</div>;
   }
-
   const book = graphQlBookToBook(graphqlBook);
-
   return <BookDetailShow book={book} />;
 };
