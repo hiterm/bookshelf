2fe4e959e02d1b1bb8b31853299d26b032c797d1
hiterm <hiterm23@gmail.com>
2026-03-20 15:54:10 +0900

    Add screenshot and video capture for failed e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ export default defineConfig({
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
   projects: [
     {
