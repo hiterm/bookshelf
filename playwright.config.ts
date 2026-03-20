ba932c16ef2ce1ee8bcc7f2e1928dc4d6f4dfdac
hiterm <hiterm23@gmail.com>
2026-03-20 14:19:22 +0900

    Enable fullyParallel in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import {
 
 export default defineConfig({
   testDir: "./e2e",
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: "html",
