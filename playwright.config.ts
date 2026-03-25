cb0985a5ea08f972987004c96f8801fee2f44bf7
hiterm <hiterm23@gmail.com>
2026-03-25 10:14:03 +0900

    Remove html reporter from Playwright configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  reporter: "html",
   use: {
     baseURL: "http://localhost:4173",
     trace: "on-first-retry",
