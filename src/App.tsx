09ba75eb8074be41a3c0db74c9378903c2837381
hiterm <hiterm23@gmail.com>
2026-03-28 01:43:37 +0900

    Use queryClient singleton in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { MantineProvider } from "@mantine/core";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { AppRoot } from "./AuthGate";
+import { queryClient } from "./lib/queryClient";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
-
-const queryClient = new QueryClient();
 
 export const App: React.FC = () => {
   return (
