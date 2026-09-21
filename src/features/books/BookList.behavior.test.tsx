ceeb6a984233f71e3f46d3ecea0bd5ab7a877a9e
hiterm <hiterm23@gmail.com>
2026-09-21 16:20:28 +0900

    Format BookList behavior test imports

--- src/features/books/BookList.behavior.test.tsx
+++ src/features/books/BookList.behavior.test.tsx
@@ -1,9 +1,4 @@
-import {
-  fireEvent,
-  screen,
-  waitFor,
-  within,
-} from "@testing-library/react";
+import { fireEvent, screen, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import {
   createBooks,
