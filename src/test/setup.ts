112c76eb3089a40e4ab236c7828ade8202ea5c10
hiterm <hiterm23@gmail.com>
2026-09-21 11:47:54 +0900

    Polyfill web streams in Vitest VM

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,9 @@
+import { ReadableStream, TransformStream, WritableStream } from "node:stream/web";
+
 import "@testing-library/jest-dom/vitest";
+
+Object.defineProperties(globalThis, {
+  ReadableStream: { configurable: true, value: ReadableStream },
+  TransformStream: { configurable: true, value: TransformStream },
+  WritableStream: { configurable: true, value: WritableStream },
+});
